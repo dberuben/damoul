2634d8b6b70c7e81a36cfa8bb5ecf39f552f5200
Daniel Beruben <daniel.beruben@ssense.com>
2016-02-22 16:29:49 -0500

    update

--- grab.sh
+++ grab.sh
@@ -1,8 +1,10 @@
-#!/bin/sh
+#!/usr/bin/bash
 
 grab_contenaire=`sudo docker images | awk {'print $1'} | grep -v REPOSITORY | head -n 1`
-grab_port=$(for i in `docker ps -a | awk {'print $1'} | grep -v CONTAINER` ; do docker port $i | awk -F: {'print $2'};done)
 
 sudo docker run -d --name $grab_contenaire -P -e APACHE_SERVERNAME=$grab_contenaire -d $grab_contenaire /usr/sbin/httpd -D FOREGROUND
 
+grab_port=$(for i in `sudo docker ps -a | awk {'print $1'} | grep -v CONTAINER` ; do sudo docker port $i | awk -F: {'print $2'};done)
+
 echo "App running on http://$grab_contenaire.ssense.com:$grab_port"
+
